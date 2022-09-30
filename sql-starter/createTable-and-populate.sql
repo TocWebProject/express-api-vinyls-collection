7da73752713b4536a125b7e3c777e7d8b204aedc
TocWebProject <rioualy@gmail.com>
2022-09-30 19:46:36 +0200

    Sql starter : new users table & foreign key Fk_id_user for vinyl table

--- sql-starter/createTable-and-populate.sql
+++ sql-starter/createTable-and-populate.sql
@@ -1,16 +1,35 @@
+CREATE TABLE users
+(
+    id_user INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    secret_password VARCHAR(255) NOT NULL,
+    registered DATE NOT NULL,
+    last_login DATE NOT NULL
+);
+
 CREATE TABLE vinyl
 (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    artiste VARCHAR(100),
+    fk_id_user INT NOT NULL,
+    FOREIGN KEY (fk_id_user) REFERENCES users (id_user) ON DELETE CASCADE,
+    artist VARCHAR(100),
     album VARCHAR(100),
     label VARCHAR(255),
-    date_de_sortie DATE
+    release_date DATE
 );
 
-INSERT INTO vinyl
-  (artiste, album, label, date_de_sortie)
+INSERT INTO users
+  (username, secret_password, registered, last_login)
 VALUES
-    ('Prince of denmark', 'V', 'Giegling', '2016-03-10'),
-    ('Simo Cell', 'Club production', 'Temet', '2021-05-23'),
-    ('Exal', 'Not to late', 'Faktice', '2022-03-13'),
-    ('Low jack', 'Yeah yo', 'Gravat', '2019-09-17');
+    ('TheUser', '$2y$10$NUQmwwEIWOdMoxYlM4jUGOVzP8Oze6HB2wVtp7arwna4Ae5nPzTla','2022-10-29', '2022-10-29');
+
+INSERT INTO vinyl
+  (artist, album, label, release_date, FK_id_user)
+VALUES
+    ('Prince of denmark', 'V', 'Giegling', '2016-03-10', '1'),
+    ('Simo Cell', 'Club production', 'Temet', '2021-05-23', '1'),
+    ('Exal', 'Not to late', 'Faktice', '2022-03-13','1'),
+    ('Low jack', 'Yeah yo', 'Gravat', '2019-09-17', '1');
+
+
+
