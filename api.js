09cd98bbf7f70905aba2feb6424621561f9ddb8b
TocWebProject <rioualy@gmail.com>
2022-10-01 15:44:20 +0200

    Main api.js: setting corsOptions

--- api.js
+++ api.js
@@ -8,10 +8,11 @@ const swaggerDocument = require('./swagger.json');
 
 const port = 3000;
 const corsOptions = {
-    origin: 'http://localhost:3000/api-docs/',
+    // Change the port in production here is local setup with Swagger Doc"
+    origin: `http://localhost:${port}/api-docs/`,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(helmet());
 
 app.use(
